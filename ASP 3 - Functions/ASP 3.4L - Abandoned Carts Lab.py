db0ccfdc3edd3cfcce39ceaeecc9002febfb569a
semanurkps@gmail.com <semanurkps@gmail.com>
2022-11-12 22:05:24 +0000

    lab 3.4 solved

--- ASP 3 - Functions/ASP 3.4L - Abandoned Carts Lab.py
+++ ASP 3 - Functions/ASP 3.4L - Abandoned Carts Lab.py
@@ -62,7 +62,8 @@ display(events_df)
 # TODO
 from pyspark.sql.functions import *
 
-converted_users_df = (sales_df.FILL_IN
+converted_users_df = (sales_df.select("email").drop_duplicates()\
+                      .withColumn("converted", lit(True))
                      )
 display(converted_users_df)
 
@@ -98,7 +99,10 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-conversions_df = (users_df.FILL_IN
+conversions_df = (users_df\
+                  .join(converted_users_df, "email", "outer")\
+                  .filter(col("email").isNotNull())\
+                  .na.fill(False)
                  )
 display(conversions_df)
 
@@ -138,7 +142,9 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-carts_df = (events_df.FILL_IN
+carts_df = (events_df.withColumn("items", explode("items"))\
+            .groupBy("user_id")\
+            .agg(collect_set("items.item_id").alias("cart"))
 )
 display(carts_df)
 
@@ -172,7 +178,7 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-email_carts_df = conversions_df.FILL_IN
+email_carts_df = conversions_df.join(carts_df, "user_id", "left")
 display(email_carts_df)
 
 # COMMAND ----------
@@ -208,8 +214,10 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-abandoned_carts_df = (email_carts_df.FILL_IN
-)
+abandoned_carts_df = (email_carts_df\
+                      .filter(col("converted") == False)\
+                      .filter(col("cart").isNotNull())
+                     )
 display(abandoned_carts_df)
 
 # COMMAND ----------
@@ -238,7 +246,10 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-abandoned_items_df = (abandoned_carts_df.FILL_IN
+abandoned_items_df = (abandoned_carts_df\
+                      .withColumn("items", explode("cart"))\
+                      .groupBy("items").count()\
+                      .sort("items")
                      )
 display(abandoned_items_df)
 
